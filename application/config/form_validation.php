b4eddcb6873a84909942a6a2ac0cd12b14a20ba2
karwackid <karwackid@slaskie.pl>
2019-05-10 07:18:42 +0200

    zmiana w stosowaniu walidacji-2

---
+++ application/config/form_validation.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array(
+
+	'users_create' => array(
+	   	array(
+			 'field'   => 'name', 
+			 'label'   => 'Imię', 
+			 'rules'   => 'trim|required'
+		),
+	   	array(
+			 'field'   => 'email', 
+			 'label'   => 'Email', 
+			 'rules'   => 'trim|required|valid_email|is_unique[users.email]'
+		),
+	   	array(
+			 'field'   => 'password', 
+			 'label'   => 'Hasło', 
+			 'rules'   => 'trim|required|matches[passconf]'
+		),   
+	   	array(
+			 'field'   => 'passconf', 
+			 'label'   => 'Potwierdzenie hasła', 
+			 'rules'   => 'trim|required'
+		),
+	),
+
+	'users_edit' => array(
+	    array(
+			 'field'   => 'name', 
+			 'label'   => 'Imię', 
+			 'rules'   => 'trim|required'
+		),
+	    array(
+			 'field'   => 'email', 
+			 'label'   => 'Email', 
+			 'rules'   => 'trim|required|valid_email|callback__unique_email'
+		),
+	    array(
+			 'field'   => 'password', 
+			 'label'   => 'Hasło', 
+			 'rules'   => 'trim|matches[passconf]'
+		),   
+	    array(
+			 'field'   => 'passconf', 
+			 'label'   => 'Potwierdzenie hasła', 
+			 'rules'   => 'trim'
+		),
+	),
+);
